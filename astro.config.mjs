ce077f73f3929a0c6022354f7b7297cc52ae430d
Alvin Penaflor <alvinpp2@gmail.com>
2024-06-14 18:10:03 +0800

    fix: vscode format on save

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,9 @@
-import { defineConfig } from 'astro/config';
-import htmx from 'astro-htmx';
+import { defineConfig } from "astro/config";
+import htmx from "astro-htmx";
 
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [htmx(), tailwind()]
+  integrations: [htmx(), tailwind()],
 });
