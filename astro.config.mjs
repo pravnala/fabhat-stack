b254205bfc1a76053e588d02e52fa2846563e697
Alvin Penaflor <alvinpp2@gmail.com>
2024-06-18 19:00:04 +0800

    feat: implement alpine js dark mode toggle
    - integrate alpine js, iconify search package, astro-icon
    - vscode: iconify search plugin, settings
    - add navbar, theme toggle

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,11 @@
 import { defineConfig } from "astro/config";
 import htmx from "astro-htmx";
-
 import tailwind from "@astrojs/tailwind";
+import icon from "astro-icon";
+
+import alpinejs from "@astrojs/alpinejs";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [htmx(), tailwind()],
+  integrations: [htmx(), tailwind(), icon(), alpinejs()]
 });
